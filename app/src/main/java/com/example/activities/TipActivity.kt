cbbab450b3ab08c01d38f62540a72cabaf8905d8
Andrews <rakdav@inbox.ru>
2024-11-25 18:03:30 +0200

    res1

--- app/src/main/java/com/example/activities/TipActivity.kt
+++ app/src/main/java/com/example/activities/TipActivity.kt
@@ -1,14 +1,15 @@
 package com.example.activities
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.activities.databinding.ActivityMainBinding
 import com.example.activities.databinding.ActivityTipBinding
+
 
 class TipActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTipBinding
@@ -36,7 +37,32 @@ class TipActivity : AppCompatActivity() {
                 setResult(RESULT_CANCELED,data)
                 finish()
             }
+            binding.buttonPhone.setOnClickListener {
+                //startActivity(Intent(Intent.ACTION_CALL, Uri.parse("+79520503549")))
+
+//                val url = "https://mail.ru"
+//                val address = Uri.parse(url)
+//                val urlIntent = Intent(Intent.ACTION_VIEW, address)
+//                if (urlIntent.resolveActivity(packageManager) != null)
+//                    startActivity(urlIntent)
+//                else Log.d("Intent", "Intent (ACTION_VIEW) не обработан!")
+
+//                val toSms="smsto:+79520503549"
+//                val mes="Masha"
+//                val sms=Intent(Intent.ACTION_SENDTO, Uri.parse(toSms))
+//                sms.putExtra("sms_body",mes)
+//                startActivity(sms)
+                val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                try {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                } catch (e: ActivityNotFoundException) {
+                    // display error state to the user
+                }
+            }
         }
+    }
+    companion object{
+     val REQUEST_IMAGE_CAPTURE=1
     }
 
 }
